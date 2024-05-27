39c89283fca639ed52a2b8ae38a551be3aa933c4
htz <1075898441@qq.com>
2024-05-27 20:40:14 +0800

    feat: 监控

--- rollup/build.js
+++ rollup/build.js
@@ -2,7 +2,7 @@
  * @Author: htz
  * @Date: 2024-05-27 17:24:44
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2024-05-27 18:00:21
+ * @LastEditTime: 2024-05-27 20:39:53
  * @Description: rollup 打包
  */
 
@@ -16,7 +16,7 @@ const resolveFile = function (filePath) {
 
 const plugins = [
   json({
-      compact: true, //忽略生成最小紧凑代码，去除不必要的空白和缩进
+    compact: true, //忽略生成最小紧凑代码，去除不必要的空白和缩进，就是的压缩格式
   }),
   babel({
     extensions: ['js', '.ts'],
@@ -34,13 +34,14 @@ const plugins = [
   }),
 ]
 
+// 陪配置了三种对象， 每一个对象代表了一种输出格式
 module.exports = [
   {
     plugins,
     input: resolveFile('../src/webEyeSDK.js'),
     output: {
       file: resolveFile('../dist/monitor.js'),
-      format: 'iife',
+      format: 'iife', // 立即执行函数格式
       name: 'monitor',
       sourcemap: true,
     },
